610524960cd778e94bebc6b9267a430c88fbd9b7
Bereket Kibru <bekakibru2@gmail.com>
2021-09-25 21:14:54 +0300

    update the file structure

--- dbt/models/example/sensor_data_observation3.sql
+++
@@ -1,6 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select *
-from {{ ref('sensor_data_observation2') }}
-where id = 1
--- dbt/models/example/sensor_data_observation4.sql
+++
@@ -1,6 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select *
-from {{ ref('sensor_data_observation1') }}
-where id = 1
