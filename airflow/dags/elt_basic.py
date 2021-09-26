60bd34e0befb7e668070162b31e6e01011d67416
Bereket Kibru <bekakibru2@gmail.com>
2021-09-26 23:22:08 +0300

    update dag scripts

--- airflow/dags/elt_basic.py
+++ airflow/dags/elt_basic.py
@@ -1,36 +1,49 @@
-from datetime import timedelta
 from airflow import DAG
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.utils.dates import datetime
-from airflow.utils.dates import timedelta
+from datetime import datetime as dt
+from datetime import timedelta
+
 
 default_args = {
-    'owner': 'bereket',
+    'owner': 'berket',
     'depends_on_past': False,
-    'start_date': datetime(2021, 10, 21),
     'email': ['bekakibru2@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
     'retries': 1,
+    'start_date': dt(2021, 9, 13),
     'retry_delay': timedelta(minutes=5)
 }
+
 dag = DAG(
     'dbt_dag',
     default_args=default_args,
-    description='An Airflow DAG to invoke simple dbt commands',
+    description='An Airflow DAG to test simple dbt',
     schedule_interval=timedelta(days=1),
+)
+dbt_debug = BashOperator(
+    task_id='dbt_debug',
+    bash_command=' cd ../../dbt && dbt debug',
+    dag=dag
 )
 
 dbt_run = BashOperator(
     task_id='dbt_run',
-    bash_command='dbt run',
+    bash_command=' cd ../../dbt && dbt run',
     dag=dag
 )
 
 dbt_test = BashOperator(
     task_id='dbt_test',
-    bash_command='dbt test',
+    bash_command='cd ../../dbt && dbt test',
+    dag=dag
+)
+dbt_doc = BashOperator(
+    task_id='dbt_doc',
+    bash_command=' cd ../../dbt && dbt docs generate',
     dag=dag
 )
 
-dbt_run >> dbt_test
+dbt_run >> dbt_test >> dbt_doc
--- airflow/dags/populate_data.py
+++ airflow/dags/populate_data.py
@@ -34,7 +34,7 @@ check_file = BashOperator(
 insert = MySqlOperator(
     task_id='insert_I80_stations',
     mysql_conn_id="mysql_conn_id",
-    sql="LOAD DATA INFILE '../../data/I80_stations.csv' INTO TABLE I80Stations FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS;",
+    sql="LOAD DATA INFILE '/var/lib/mysql-files/I80_stations.csv' INTO TABLE I80Stations FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS (ID,Fwy,Dir,District,County,City,@vState_PM,Abs_PM,Latitude,Longitude,@vLength,Type,Lanes,Name,User_ID_1,User_ID_2,User_ID_3,User_ID_4) SET State_PM = NULLIF( @vState_PM, ''),Length = NULLIF( @vLength, '');",
     dag=dag
 )
 
