97e95a560571ee7bddbc1f6b632c07d489f5369f
Bereket Kibru <bekakibru2@gmail.com>
2021-09-27 19:58:22 +0300

    update the model

--- dbt/models/example/sensor_data.sql
+++ dbt/models/example/sensor_data.sql
@@ -1,27 +1,10 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
 {{ config(materialized='table') }}
 
-with source_data as (
+with final as (
 
-    select 1 as id
-    union all
-    select null as id
+    SELECT ID, FWY, DIR, District, County, City, State_PM, Abs_PM, Latitude, Longitude, Length, Type, Lanes, Name
+    FROM dbtdb.I80Stations
 
 )
 
-select *
-from source_data
-where id is not null
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
+SELECT * FROM final
